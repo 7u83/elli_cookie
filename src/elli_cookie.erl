84264d51475ac5ecdaa43f754572780f9ecb89b3
Eric Bailey <eric@ericb.me>
2016-03-26 02:06:13 -0500

    Update README, indentation and tests
    Convert README.md to Org and make minor updates,
    such as s/elli_cookie/elli_cookie_test/.
    
    Set erlang-indent-level to 2.
    
    Rename elli_test to elli_cookie_test, oops.

--- src/elli_cookie.erl
+++ src/elli_cookie.erl
@@ -28,36 +28,36 @@ parse(Req = #req{}) -> tokenize(elli_request:get_header(<<"Cookie">>, Req)).
 %% gets a specific cookie value from the set of parsed cookie
 -spec get(Key :: binary(), Cookies :: cookie_list()) -> undefined | binary().
 get(_, no_cookies) ->
-    undefined;
+  undefined;
 get(Key, Cookies) ->
-    ok = valid_cookie_name(Key),
-    proplists:get_value(to_bin(Key), Cookies).
+  ok = valid_cookie_name(Key),
+  proplists:get_value(to_bin(Key), Cookies).
 
 -spec get(Key :: binary(), Cookies :: cookie_list(), Default) -> Default | binary().
 get(_, no_cookies, Default) ->
-    Default;
+  Default;
 get(Key, Cookies, Default) ->
-    ok = valid_cookie_name(Key),
-    proplists:get_value(to_bin(Key), Cookies, Default).
+  ok = valid_cookie_name(Key),
+  proplists:get_value(to_bin(Key), Cookies, Default).
 
 %% creates a new cookie in a format appropriate for server response
 -spec new(Name :: stringy(), Value :: stringy()) -> cookie().
 new(Name, Value) ->
-    ok = valid_cookie_name(Name),
-    ok = valid_cookie_value(Value),
-    BName = to_bin(Name),
-    BVal = to_bin(Value),
-    {<<"Set-Cookie">>, <<BName/binary, "=", BVal/binary>>}.
+  ok = valid_cookie_name(Name),
+  ok = valid_cookie_value(Value),
+  BName = to_bin(Name),
+  BVal = to_bin(Value),
+  {<<"Set-Cookie">>, <<BName/binary, "=", BVal/binary>>}.
 
 -spec new(Name :: stringy(), Value :: stringy(), Options :: [cookie_option()]) -> cookie().
 new(Name, Value, Options) ->
-    ok = valid_cookie_name(Name),
-    ok = valid_cookie_value(Value),
-    BName = to_bin(Name),
-    BValue = to_bin(Value),
-    Bin = <<BName/binary,"=",BValue/binary>>,
-    FinalBin = lists:foldl(fun set_cookie_attribute/2, Bin, Options),
-    {<<"Set-Cookie">>, FinalBin}.
+  ok = valid_cookie_name(Name),
+  ok = valid_cookie_value(Value),
+  BName = to_bin(Name),
+  BValue = to_bin(Value),
+  Bin = <<BName/binary,"=",BValue/binary>>,
+  FinalBin = lists:foldl(fun set_cookie_attribute/2, Bin, Options),
+  {<<"Set-Cookie">>, FinalBin}.
 
 %% Creates a header that will delete a specific cookie on the client
 -spec delete(Name :: stringy()) -> cookie().
@@ -65,8 +65,8 @@ delete(Name) -> delete(Name, []).
 
 -spec delete(Name :: stringy(), Options :: [cookie_option()]) -> cookie().
 delete(Name, Options) ->
-    ok = valid_cookie_name(Name),
-    new(Name, "", [expires({{1970,1,1},{0,0,0}}) | Options]).
+  ok = valid_cookie_name(Name),
+  new(Name, "", [expires({{1970,1,1},{0,0,0}}) | Options]).
 
 
 %%%===================================================================
@@ -110,45 +110,45 @@ to_bin(L) when is_list(L)   -> list_to_binary(L);
 to_bin(X)                   -> throw({error, {not_a_string, X}}).
 
 tokenize(<<>>) ->
-    [];
+  [];
 tokenize(CookieStr) when is_binary(CookieStr) ->
-    Cookies = binary:split(CookieStr, <<";">>, [trim, global]),
-    lists:map(fun tokenize2/1, Cookies);
+  Cookies = binary:split(CookieStr, <<";">>, [trim, global]),
+  lists:map(fun tokenize2/1, Cookies);
 tokenize(_) ->
-    no_cookies.
+  no_cookies.
 
 tokenize2(NVP) ->
-    [N,V] = binary:split(NVP, <<"=">>, [trim]),
-    {strip_bin(N), strip_bin(V)}.
+  [N,V] = binary:split(NVP, <<"=">>, [trim]),
+  {strip_bin(N), strip_bin(V)}.
 %% {N, V}.
 
 set_cookie_attribute({expires, Exp}, Bin) ->
-    BExp = to_bin(Exp),
-    <<Bin/binary, ";Expires=", BExp/binary>>;
+  BExp = to_bin(Exp),
+  <<Bin/binary, ";Expires=", BExp/binary>>;
 set_cookie_attribute({max_age, Exp}, Bin) ->
-    BExp = to_bin(integer_to_list(Exp)),
-    <<Bin/binary, ";Max-Age=", BExp/binary>>;
+  BExp = to_bin(integer_to_list(Exp)),
+  <<Bin/binary, ";Max-Age=", BExp/binary>>;
 set_cookie_attribute({path, Path}, Bin) ->
-    BPath = to_bin(Path),
-    <<Bin/binary, ";Path=", BPath/binary>>;
+  BPath = to_bin(Path),
+  <<Bin/binary, ";Path=", BPath/binary>>;
 set_cookie_attribute({domain, Domain}, Bin) ->
-    BDomain = to_bin(Domain),
-    <<Bin/binary, ";Domain=", BDomain/binary>>;
+  BDomain = to_bin(Domain),
+  <<Bin/binary, ";Domain=", BDomain/binary>>;
 set_cookie_attribute(secure, Bin) ->
-    <<Bin/binary, ";Secure">>;
+  <<Bin/binary, ";Secure">>;
 set_cookie_attribute(http_only, Bin) ->
-    <<Bin/binary, ";HttpOnly">>;
+  <<Bin/binary, ";HttpOnly">>;
 set_cookie_attribute(X, _) ->
-    throw({error, {invalid_cookie_attribute, X}}).
+  throw({error, {invalid_cookie_attribute, X}}).
 
 expires_plus(N) ->
-    UT = calendar:datetime_to_gregorian_seconds(calendar:universal_time()),
-    UTE = UT + N,
-    Date = calendar:gregorian_seconds_to_datetime(UTE),
-    {expires, httpd_util:rfc1123_date(Date)}.
+  UT = calendar:datetime_to_gregorian_seconds(calendar:universal_time()),
+  UTE = UT + N,
+  Date = calendar:gregorian_seconds_to_datetime(UTE),
+  {expires, httpd_util:rfc1123_date(Date)}.
 
 strip_bin(B) ->
-    list_to_binary(string:strip(binary_to_list(B))).
+  list_to_binary(string:strip(binary_to_list(B))).
 
 
 %%%===================================================================
@@ -157,18 +157,18 @@ strip_bin(B) ->
 
 %% TODO: implement cookie spec checking: https://tools.ietf.org/html/rfc6265
 valid_cookie_name(B) when is_binary(B) ->
-    Str = binary_to_list(B),
-    valid_cookie_name2(string:str(Str, "="), B);
+  Str = binary_to_list(B),
+  valid_cookie_name2(string:str(Str, "="), B);
 valid_cookie_name(N) when is_list(N) ->
-    valid_cookie_name2(string:str(N, "="), N);
+  valid_cookie_name2(string:str(N, "="), N);
 valid_cookie_name(X) ->
-    {invalid_cookie_name, X}.
+  {invalid_cookie_name, X}.
 
 valid_cookie_name2(0, _) -> ok;
 valid_cookie_name2(_, N) -> {invalid_cookie_name, N}.
 
 %% TODO: implement cookie spec checking: https://tools.ietf.org/html/rfc6265
 valid_cookie_value(B) when is_binary(B); is_list(B) ->
-    ok;
+  ok;
 valid_cookie_value(X) ->
-    {invalid_cookie_value, X}.
+  {invalid_cookie_value, X}.
